abc4d2d975fda27dd08be3e69658dcf63d9ca633
Alena Trushnikova <alena.yu.t@gmail.com>
2020-10-28 22:18:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -69,3 +69,23 @@ def run(songs)
 end
 =end
 
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
+end
+
